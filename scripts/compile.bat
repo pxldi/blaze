86aa32a1a6fe42479816a34d9a4167caf3a9d333
Pxldi <leopold.sappler@gmail.com>
2024-11-01 00:13:22 +0100

    Add graphics pipeline and renderpass creation

---
+++ scripts/compile.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ../src/shaders/shader.vert -o ../src/shaders/vert.spv
+C:\VulkanSDK\1.3.296.0\Bin\glslc.exe ../src/shaders/shader.frag -o ../src/shaders/frag.spv
+pause
